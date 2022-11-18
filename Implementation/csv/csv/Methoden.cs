c4809677a863fd565ea8fc212a04159389d18a62
MorsPugnator <117175652+MorsPugnator@users.noreply.github.com>
2022-11-18 16:39:16 +0100

    Add files via upload

---
+++ Implementation/csv/csv/Methoden.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csv
+{
+    class Methoden
+    {
+        static string Datei = @"C:\Users\yanre\Code\csv.txt";
+
+        List<string> Einkaufszettel = new List<string>();
+        List<decimal> Kilo = new List<decimal>();
+        public void ExistiertDatei()
+        {
+            if (File.Exists(Datei))
+            {
+                File.Delete(Datei);
+            }
+
+            using (FileStream fs = File.Create(Datei))
+            {
+
+            }
+        }
+        public void AddDaten(string gemüse,string Herkunft, decimal preis)
+        {
+            string einfuegen = gemüse + ";" + Herkunft + ";" + preis.ToString() + ";";
+            using (StreamWriter sw = File.AppendText(Datei))
+            {
+                sw.WriteLine(einfuegen);
+            }
+        }
+
+        public void GemüseSortiement()
+        {
+            using (StreamReader sr = new StreamReader(Datei, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var values = line.Split(";");
+                    string a = values[0];
+                    string b = values[1];
+                    string c = values[2];
+                    Console.WriteLine("{0,-20} {1,0} {2,-15} {3,0} {4,0}", a, " Herkunft: ", b, " Preis:", c);
+                }
+                
+            }
+            
+        }
+
+        public void GemüseAdd(string Gemüse, decimal KG)
+        {
+            Einkaufszettel.Add(Gemüse);
+            Kilo.Add(KG/1000);
+        }
+
+        public bool Gemüsefinden(string gemüse)
+        {
+                using (StreamReader sr = new StreamReader(Datei, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var values = line.Split(";");
+                        string a = values[0];
+                        if (string.Equals(gemüse, a, StringComparison.OrdinalIgnoreCase))
+                        {
+                        return true;   
+                        }
+                    }
+                }
+            return false;
+        }
+        public void PrintEinkaufszettel()
+        {
+            decimal gesamtpreis = 0;
+            var date = DateTime.Now;
+            Console.WriteLine("{0,25}", "GemüseHandel Otto");
+            Console.WriteLine("{0, 48}", date.ToString("dd,MM,yyyy"));
+            Console.WriteLine();
+            foreach (var item in Einkaufszettel)
+            {
+                int i = 0;
+                string c;
+                decimal g;
+                
+                using (StreamReader sr = new StreamReader(Datei, Encoding.UTF8))
+                { 
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var values = line.Split(";");
+                        string a = values[0];
+                        c = values[2];
+                        if (string.Equals(item, a, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("{0,-15} {1,0} {2,-5} {3,0} {4,0} {5,0}", a,  Kilo[i], "KG", " Preis pro KG:", c, "EUR");
+                            g = Convert.ToDecimal(c);
+                            g = Kilo[i] * g;
+                            gesamtpreis = gesamtpreis + g;
+                            break;
+                        }
+                        
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0,39} {1,0} {2,0}","Gesamtpreis:", gesamtpreis, "EUR");
+        }
+    }
+
+}
+
---
+++ Implementation/csv/csv/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace csv
+{
+    class Program
+    {
+        static Methoden d = new Methoden();
+        static void Main(string[] args)
+        {
+            int i = 0;
+            d.ExistiertDatei();
+
+            d.AddDaten("Auberginen", "Spanien", 3.99m);
+            d.AddDaten("Bataviasalat", "Frankreich", 1.99m);
+            d.AddDaten("Blumenkohl", "Deutschland", 3.79m);
+            d.AddDaten("Koriander", "Deutschland", 5.49m);
+            d.AddDaten("Kurkuma", "Peru", 9.00m);
+            d.AddDaten("Lauch", "Deutschland", 3.99m);
+            d.AddDaten("Möhren", "Deutschland", 2.19m);
+            d.AddDaten("Möhren bunt", "Deutschland", 2.49m);
+            d.AddDaten("Paprika Rot", "Spanien", 5.99m);
+            d.AddDaten("Pastinaken", "Deutschland", 3.99m);
+            d.AddDaten("Petersilie", "Deutschland", 8.99m);
+            d.AddDaten("Rettich schwarz", "Deutschland", 2.99m);
+            d.AddDaten("Rettich weiß", "Deutschland", 1.99m);
+            d.AddDaten("Rote Bete", "Deutschland", 2.39m);
+            d.AddDaten("Rote Bete gegart", "Deutschland", 4.38m);
+            d.AddDaten("Rotkohl", "Deutschland", 2.49m);
+            d.AddDaten("Salatgurke", "Spanien", 4.99m);
+            d.AddDaten("Sellerie", "Deutschland", 2.99m);
+            d.AddDaten("Snackmöhren", "Dänemark", 2.99m);
+            d.AddDaten("Spitzkohl", "Deutschland", 2.69m);
+            d.AddDaten("Spitzpaprika", "Spanien", 7.00m);
+            d.AddDaten("Sprossem diverse", "Deutschland", 8.69m);
+
+            d.GemüseSortiement();
+            do
+            {
+                string f;
+                string h;
+                Console.WriteLine("Geben sie den Namen des Artikel ein den Sie kaufen möchten.");
+                f = Console.ReadLine();
+                Console.WriteLine("Wie viel gramm wollen sie kaufen?");
+                h = Console.ReadLine();
+                if (d.Gemüsefinden(f) == false)
+                {
+                    do
+                    {
+                        Console.WriteLine("Haben sie sich vertippt das Gemüse wurde nicht gefunden.");
+                        Console.WriteLine("Bitte geben sie es erneut ein.");
+                        f = Console.ReadLine();
+                    } while (d.Gemüsefinden(f) == false);
+                }
+
+                d.GemüseAdd(f, Convert.ToDecimal(h));
+                if(i == 5)
+                {
+                    Console.WriteLine("Wollen sie die Liste erneut sehen? Antworten Sie mit ja oder nein");
+                    if(Console.ReadLine() == "ja" || Console.ReadLine() == "Ja")
+                    {
+                        d.GemüseSortiement();
+                        i = -1;
+                    }
+                }
+                Console.WriteLine("Wollen sie einen weitern Artikel Kaufen dann antworten sie mit ja, wenn sie nichts weiteres brauchen mit nein");
+                i++;
+            }
+            while (Console.ReadLine() != "nein");
+            d.PrintEinkaufszettel();
+
+            Console.ReadLine();
+        }
+    }
+}
